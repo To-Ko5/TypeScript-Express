2cc3ad8bce59b64dc5f9b2cace0eef5c3d2cd77a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-28 08:25:13 +0900

    user add

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,14 @@ app.get('/about', (req, res, next) => {
   res.render('about.ejs')
 })
 
+app.get('/user/:name?', (req, res, next) => {
+  if (req.params.name) {
+    res.send(req.params.name)
+  } else {
+    res.send('no user')
+  }
+})
+
 app.post('/', (req, res, next) => {
   console.log(req.body)
   res.send(`<h1>${req.body}</h1>`)
