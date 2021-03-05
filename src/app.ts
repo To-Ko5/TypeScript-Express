761ffcb29f9586ecd772264269cf5a29d31b5743
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-06 08:20:53 +0900

    url add

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.use((req, res, next) => {
-  console.log('hello')
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
   next()
 })
 
